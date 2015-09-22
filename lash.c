658918288ab7ec332bbb94a6d6bdfe048614b8f8
Alex Maras <alexmaras@gmail.com>
2015-09-22 21:31:15 +0800

    Add exit clause, prompt and loop until exit entered functionality

--- lash.c
+++ lash.c
@@ -1,11 +1,37 @@
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
 
+#define MAXLENGTH 1000 
 
-int main(){
+char *prompt = "\nLaSH % ";
+static volatile int keepRunning = 1;
 
-    printf("Hi there");
+void intHandler(int dummy) {
+    keepRunning = 0;
+}
+
+void printPrompt(){
+    printf("%s", prompt);
+}
+
+int main(void){
+    // define signal handler to prevent ctrl-c from killing the process
+    signal(SIGINT, intHandler);
+    char *input = (char*) malloc( MAXLENGTH );
+    strcpy(input, "");
+    size_t ln;
+    
+    while(strcmp("exit", input) != 0){
+        printPrompt();
+        fgets(input, MAXLENGTH, stdin);
+        ln = strlen(input) - 1;
+        if(input[ln] == '\n'){
+            input[ln] = '\0';
+        }
+    }
 
     exit(0);
 }
