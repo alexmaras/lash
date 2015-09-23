752f9b7de6a5e97655e229e735c3d5324a18da55
Alex Maras <alex.maras@wanews.com.au>
2015-09-23 09:15:47 +0800

    Start splitting of arguments and commands

--- lash.c
+++ lash.c
@@ -4,9 +4,12 @@
 #include <string.h>
 #include <signal.h>
 
-#define MAXLENGTH 1000 
+#define MAXCOMMANDS 100
+#define MAXARGS 1000
+#define MAXARGLENGTH 100
 
-char *prompt = "\nLaSH % ";
+char prompt[80];
+
 static volatile int keepRunning = 1;
 
 void intHandler(int dummy) {
@@ -17,27 +20,54 @@ void printPrompt(){
     printf("%s", prompt);
 }
 
+void trimEndingNewline(char *line){
+	size_t length = strlen(line);
+	if(line[length-1] == '\n')
+		line[length-1] = '\0';
+}
+
+void parseCommands(char *line, char *commands){
+	//char *argument = "hello there";
+	//memcpy(commands, argument, 11);
+}
+
 int main(void){
-    // define signal handler to prevent ctrl-c from killing the process
-    signal(SIGINT, intHandler);
-    char *input = (char*) malloc( MAXLENGTH );
+
+	sprintf(prompt, "%s LaSH %% ", getenv("USER"));
+
+    // ignore all signals that should be passed to jobs
+    signal (SIGINT, SIG_IGN);
+    signal (SIGQUIT, SIG_IGN);
+   	signal (SIGTSTP, SIG_IGN);
+   	signal (SIGTTIN, SIG_IGN);
+   	signal (SIGTTOU, SIG_IGN);
+   	signal (SIGCHLD, SIG_IGN);
+
+	int linelength =  MAXCOMMANDS * MAXARGS * MAXARGLENGTH;
+    char *input = (char*) malloc( linelength );
+
     strcpy(input, "");
-    size_t ln;
-    
-    while(strcmp("exit", input) != 0){
+
+	char commandArray[ MAXCOMMANDS ][ MAXARGS ][ MAXARGLENGTH ];
+
+	// Loop forever. This will be broken if exit is run
+    while(1){
         printPrompt();
-        fgets(input, MAXLENGTH, stdin);
-        ln = strlen(input) - 1;
-        if(input[ln] == '\n'){
-            input[ln] = '\0';
-        }
-    
-        if(strcmp("exit", input) != 0 &&
-           strcmp("pwd", input) != 0 &&
-           strcmp("cd", input) != 0)
-        {
-            system(input);
-        }
+        fgets(input, linelength, stdin);
+
+		// cut newline off and replace with null
+		trimEndingNewline(input);
+
+		parseCommands(input, &commandArray[0][0][0]);
+
+		printf("%s", commandArray[0][0]);
+
+		if(strcmp("exit", input) == 0)
+			break;
+
+		if(strcmp("pwd", input) == 0){
+
+		}
 
     }
 
